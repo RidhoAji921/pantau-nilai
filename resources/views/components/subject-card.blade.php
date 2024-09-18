6ba5dba441a7ff64b72bb3c0cdd78eb3d5bf9819
RidhoAji921 <ridhoaji921@gmail.com>
2024-09-18 10:45:35 +0700

    nambah tooltip

--- resources/views/components/subject-card.blade.php
+++ resources/views/components/subject-card.blade.php
@@ -1,6 +1,6 @@
 <div class="border-2 border-black p-1 rounded-lg">
     <div class="flex justify-between">
-        <h3 class="font-semibold text-xl truncate hover:underline">{{ $name }}</h3>
+        <h3 class="font-semibold text-xl truncate hover:underline" title="{{ $name }}">{{ $name }}</h3>
         <h3>{{ $code }}</h3>
     </div>
     <h2 class="font-semibold text-sm hover:underline">{{ $lecturer }}</h3>
